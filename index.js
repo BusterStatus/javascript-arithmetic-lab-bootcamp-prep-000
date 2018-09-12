1259dd028cbc3d37b1c884fb3bb804dafaad998c
matthew.serre@gmail.com <matthew.serre@gmail.com>
2018-09-12 00:24:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,17 +15,17 @@ function divide (a,b) {
 }
 
 function inc (n) {
-  return a++;
+  return n++;
 }
 
 function dec (n) {
-  return a--;
+  return n--;
 }
 
 function makeInt (n) {
-  return parseInt(a, 10);
+  return parseInt(n, 10);
 }
 
 function preserveDecimal (n) {
-  return parseFloat(a);
+  return parseFloat(n);
 }
